c7f5a989a71d48f9990c9ff718c0a11b7286d381
Thomas Battiston <thomas.battiston@wttj.co>
2021-12-01 20:34:23 +0100

    feat: red test for day one challenge (p2)

--- test/day_one_test.exs
+++ test/day_one_test.exs
@@ -1,30 +1,38 @@
 defmodule AdventOfCode.DayOneTest do
   use ExUnit.Case
 
+  setup do
+    steps = [
+      199,
+      200,
+      208,
+      210,
+      200,
+      207,
+      240,
+      269,
+      260,
+      263
+    ]
+
+    {:ok, %{steps: steps}}
+  end
+
   alias AdventOfCode.DayOne
 
   describe "sonar_depth/1" do
-    setup do
-      steps = [
-        199,
-        200,
-        208,
-        210,
-        200,
-        207,
-        240,
-        269,
-        260,
-        263
-      ]
-
-      {:ok, %{steps: steps}}
-    end
-
     test "returns the number of times the depth increased compared to the previous one", %{
       steps: steps
     } do
       assert DayOne.sonar_depth(steps) == 7
+    end
+  end
+
+  describe "sonar_depth_evolved/1" do
+    test "returns the number of times the depth increased compared to the previous one", %{
+      steps: steps
+    } do
+      assert DayOne.sonar_depth_evolved(steps) == 5
     end
   end
 end
