0228be7a02cfa99556b17de6f630eb41461b7e3a
Thomas Battiston <thomas.battiston@wttj.co>
2021-12-02 17:56:51 +0100

    feat: green for day two (p2)

--- lib/day_two.ex
+++ lib/day_two.ex
@@ -9,7 +9,7 @@ defmodule AdventOfCode.DayTwo do
   def run(input_path) do
     input_path
     |> parse_file()
-    |> move()
+    |> move_evolved()
   end
 
   def move(steps) do
@@ -17,11 +17,14 @@ defmodule AdventOfCode.DayTwo do
     |> Enum.map(&parse/1)
     |> Enum.reduce([position: 0, depth: 0], &follow_step/2)
     |> final_value()
-
-    # |> Enum.reduce([depth: 0, position: 0], &follow_step/2)
   end
 
-  def move_evolved(_steps), do: nil
+  def move_evolved(steps) do
+    steps
+    |> Enum.map(&parse/1)
+    |> Enum.reduce([position: 0, depth: 0, aim: 0], &follow_step/2)
+    |> final_value()
+  end
 
   defp parse(step) do
     {_, move} =
@@ -35,15 +38,26 @@ defmodule AdventOfCode.DayTwo do
     }
   end
 
-  defp follow_step({:forward, value}, acc),
-    do: [position: acc[:position] + value, depth: acc[:depth]]
+  defp follow_step({:forward, value}, position: position, depth: depth),
+    do: [position: position + value, depth: depth]
 
-  defp follow_step({:down, value}, acc),
-    do: [position: acc[:position], depth: acc[:depth] + value]
+  defp follow_step({:down, value}, position: position, depth: depth),
+    do: [position: position, depth: depth + value]
 
-  defp follow_step({:up, value}, acc), do: [position: acc[:position], depth: acc[:depth] - value]
+  defp follow_step({:up, value}, position: position, depth: depth),
+    do: [position: position, depth: depth - value]
+
+  defp follow_step({:forward, value}, position: position, depth: depth, aim: aim),
+    do: [position: position + value, depth: depth + aim * value, aim: aim]
+
+  defp follow_step({:down, value}, position: position, depth: depth, aim: aim),
+    do: [position: position, depth: depth, aim: aim + value]
+
+  defp follow_step({:up, value}, position: position, depth: depth, aim: aim),
+    do: [position: position, depth: depth, aim: aim - value]
 
   defp final_value(position: position, depth: depth), do: position * depth
+  defp final_value(position: position, depth: depth, aim: _aim), do: position * depth
 
   defp parse_file(path) do
     path
