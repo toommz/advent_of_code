eabed233c02e5dfad02adede67ea30e95408635d
Thomas Battiston <thomas.battiston@wttj.co>
2021-12-07 10:20:28 +0100

    feat: green for day three (p1)

--- lib/day_three.ex
+++ lib/day_three.ex
@@ -14,5 +14,41 @@ defmodule AdventOfCode.DayThree do
     end
   end
 
-  def power_consumption(_data), do: nil
+  def power_consumption(data) do
+    data
+    |> Enum.map(&String.split(&1, ~r/.{1}/, trim: true, include_captures: true))
+    |> Enum.zip()
+    |> Enum.map(&count_duplicates/1)
+    |> Enum.map(&find_epsilons_and_gammas/1)
+    |> Enum.zip()
+    |> Enum.map(&build_bitstring/1)
+    |> calculate_final_value()
+  end
+
+  defp count_duplicates(duplicates) do
+    duplicates
+    |> Tuple.to_list()
+    |> Enum.reduce({0, 0}, fn el, acc ->
+      case el do
+        "0" -> {elem(acc, 0) + 1, elem(acc, 1)}
+        "1" -> {elem(acc, 0), elem(acc, 1) + 1}
+      end
+    end)
+  end
+
+  defp find_epsilons_and_gammas({zeros, ones}) do
+    case zeros > ones do
+      true -> ["0", "1"]
+      false -> ["1", "0"]
+    end
+  end
+
+  defp build_bitstring(tuple) do
+    tuple
+    |> Tuple.to_list()
+    |> Enum.map(&String.to_integer/1)
+    |> Integer.undigits(2)
+  end
+
+  defp calculate_final_value([epsilon, gamma]), do: epsilon * gamma
 end
